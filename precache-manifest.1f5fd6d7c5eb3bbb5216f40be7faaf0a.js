a79004da91d1713dfca125df0cf581405a1da344
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-09-03 17:03:18 -0300

    Updates

---
+++ precache-manifest.1f5fd6d7c5eb3bbb5216f40be7faaf0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2e8d63915222d6c051069bf64935d3",
+    "url": "/Api-lol/index.html"
+  },
+  {
+    "revision": "9a6a461d48e9cd35cbc4",
+    "url": "/Api-lol/static/js/2.983c6c7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Api-lol/static/js/2.983c6c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b52827e5e64a54fc967",
+    "url": "/Api-lol/static/js/main.0b560e74.chunk.js"
+  },
+  {
+    "revision": "ee6c143d135b4a8e4465",
+    "url": "/Api-lol/static/js/runtime-main.21d3c5a1.js"
+  }
+]);
